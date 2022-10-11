2dd1167f5fc9bceccfb270647d19273194a06517
joerdixon <joseph35dixon@gmail.com>
2022-10-11 15:40:46 -0700

    added modal and add item buttons

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,26 +1,57 @@
 // GLOBAL VARIABLES -------------------------------------------------------
 
 // DOM SELECTORS ----------------------------------------------------------
-var searchBar = document.getElementById("search-bar");
-var searchButton = document.getElementById("search-button");
+var addItemOne = document.getElementById("add-item-one");
+var addItemTwo = document.getElementById("add-item-two");
 var cardImg = document.getElementById("card-img");
 
 // FUNCTIONS --------------------------------------------------------------
 
 // EVENT LISTENERS --------------------------------------------------------
 
-// SCRYFALL API
-function getPrice(card) {
-    fetch("https://api.scryfall.com/cards/named?order=usd&unique=prints&fuzzy=" + card).then(
-        function (response) {
-        return response.json();
-      }).then(function (data) {
-        console.log(data);
-        cardImg.setAttribute("src", data.image_uris.normal);
-        cardImg.setAttribute("alt", data.name); 
-    });
-}
-
-searchButton.addEventListener("click", function () {
-    getPrice(searchBar.value);
+addItemOne.addEventListener("click", function () {
+    openModal();
 });
+
+addItemTwo.addEventListener("click", function () {
+    openModal();    
+})
+
+// NEW JAVASCRIPT FROM JRD-MODAL-BUTTON (BULMA)
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Functions to open and close a modal
+    function openModal($el) {
+      $el.classList.add('is-active');
+    }
+  
+    function closeModal($el) {
+      $el.classList.remove('is-active');
+    }
+  
+    function closeAllModals() {
+      (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+        closeModal($modal);
+      });
+    }
+  
+    // Add a click event on buttons to open a specific modal
+    (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+      const modal = $trigger.dataset.target;
+      const $target = document.getElementById(modal);
+  
+      $trigger.addEventListener('click', () => {
+        openModal($target);
+      });
+    });
+  
+    // Add a click event on various child elements to close the parent modal
+    (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+      const $target = $close.closest('.modal');
+  
+      $close.addEventListener('click', () => {
+        closeModal($target);
+      });
+    });
+
+  });
