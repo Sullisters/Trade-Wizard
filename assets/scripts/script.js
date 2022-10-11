4cb1c27c612c6cb7d39a6da040807b4f9ea9351f
joerdixon <joseph35dixon@gmail.com>
2022-10-11 15:08:56 -0700

    js pseudocode

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -41,7 +41,7 @@ Cards on list (once added)
 When I click add item:
     A modal with a search bar and other options appears.
     I can search cards and adjust their condition and foil status.
-    When I search for a card:
+    When I search for a card in the search bar:
         It appears in the preview on the modal.
     When I confirm my selection:
         The modal closes.
@@ -53,6 +53,11 @@ When I click a card on either list:
 
 When I click the X on a list item:
     That card is removed from the trade.
+
+When I look at the trade summary section:
+    I see both me and my partners total value added up.
+    I see the difference (if any) between our values.
+    There is a gradient representation of our values (mvp+)
 */
 
 
